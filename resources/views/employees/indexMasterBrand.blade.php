30a6412dfa96b5c4a10ba0d4b2488fb6299ac4ea
Simon071198 <81679661+Simon071198@users.noreply.github.com>
2026-07-06 21:53:07 +0700

    Membuat background image halaman awal dan menampilkan icon favicon ramayana dalam setiap head title

--- resources/views/employees/indexMasterBrand.blade.php
+++ resources/views/employees/indexMasterBrand.blade.php
@@ -1,6 +1,7 @@
 @extends('adminlte::page')
 
 @section('title', 'Master Brand')
+@extends('icon')
 
 @section('content_header')
     <div class="container-fluid">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,8 @@
 
 @section('title', 'Dashboard')
 
+@extends('icon')
+
 @section('content_header')
     <h1>Dashboard</h1>
 @stop
